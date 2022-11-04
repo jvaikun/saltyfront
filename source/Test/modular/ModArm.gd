eeb1790cf4b903baafa4bceccc2b6aa209b0be6d
jvaikun <jeevan.vaikun@gmail.com>
2022-11-04 23:34:07 +0700

    Resource rearranging

--- source/Test/modular/ModArm.gd
+++ source/Test/modular/ModArm.gd
@@ -6,7 +6,7 @@ onready var anim_legs = $mech_legs/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim_idle()
+	anim_legs.play("reset") #anim_idle()
 	pass # Replace with function body.
 
 
